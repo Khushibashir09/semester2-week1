d736d556f7c16194fda15ba94ca2be25beeed94c
Khushibashir09 <wtms0841@leeds.ac.uk>
2026-01-26 12:48:17 +0000

    2.1.1

--- session_1/1_country/task1.sql
+++ session_1/1_country/task1.sql
@@ -11,5 +11,17 @@
 
 
 -- write your sql code here
+SELECT * FROM countries;
+SELECT Country, Population FROM countries; 
+SELECT Country FROM countries WHERE Continent = 'Europe';
+SELECT Country FROM countries WHERE Population > 100000000;
+SELECT Country FROM countries WHERE Continent = 'Asia' AND GDPPerCapita > 20000; 
+SELECT Country FROM countries WHERE LiteracyPercent < 70;
+SELECT Country FROM countries WHERE CoastlineRation = 0.0;
+SELECT Country FROM countries WHERE Country LIKE 'A%';
+SELECT Country FROM countries WHERE Country LIKE '%United%';
+SELECT Country FROM countries WHERE Birthrate > Deathrate AND Population > 50000000;
+
+
 
 SELECT * from countries;
--- session_1/1_country/task2.sql
+++ session_1/1_country/task2.sql
@@ -9,6 +9,12 @@
 -- 2. Load this script: .read task2.sql
 -- 3. Exit SQLite: .exit
 
+SELECT Country FROM countries ORDER BY Population DESC LIMIT 10;
+SELECT Country FROM countries ORDER BY AreaSqMi ASC LIMIT 5;
+SELECT Country FROM countries WHERE Continent = 'Europe' ORDER BY AreaSqMi Desc LIMIT 1;
+SELECT Country FROM countries WHERE Continent = 'Africa'  ORDER BY AreaSqMi  ASC LIMIT 3;
+SELECT Country FROM countries ORDER BY LiteracyPercent ASC LIMIT 5;
+SELECT Country FROM countries WHERE Country LIKE 'C%' ORDER BY GDPPerCapita LIMIT 3;
 
 -- write your sql code here
 
